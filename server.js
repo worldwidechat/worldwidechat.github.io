cf2f986e68801dbc171db38e97ac5fa20e62480b
worldwidechat <70862076+worldwidechat@users.noreply.github.com>
2020-09-08 23:33:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ io.on('connection', socket =>{
     })
 
 })
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log('listening on *:'+ PORT);
   });
